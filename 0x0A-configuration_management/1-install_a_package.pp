820b4fd3a3539f244b9471ffdddb0cb9e146bc30
Bimacobiz <collinsmahigi@gmail.com>
2023-11-29 07:14:01 -0800

    changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
-# puppet declarative script to install flask from pip3.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# install_flask.pp
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
+  ensure  => '2.1.0',
+  require => Package['python3-pip'],
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => '/usr/local/bin',
+  refreshonly => true,
+  creates     => '/usr/local/lib/python3.8/dist-packages/Flask-2.1.0.dist-info',
 }
