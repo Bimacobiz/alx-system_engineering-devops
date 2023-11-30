d05dd7061d4bbc34be40b640de6f043943b509d9
Bimacobiz <collinsmahigi@gmail.com>
2023-11-29 16:34:54 -0800

    changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,6 @@
-# install_flask.pp
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-package { 'Flask':
-  ensure  => '2.1.0',
-  require => Package['python3-pip'],
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => '/usr/local/bin',
-  refreshonly => true,
-  creates     => '/usr/local/lib/python3.8/dist-packages/Flask-2.1.0.dist-info',
-}
+#!/usr/bin/pup
+#Install a specified version of Flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0'.
+  provider => 'pip3'
+{
