93e4a125c7267aa6dc3be5023b05f78deccd1882
Bimacobiz <collinsmahigi@gmail.com>
2023-11-30 12:09:54 -0800

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-
-# Use curl to send a request and get the body, then use wc to count the bytes
-body_size=$(curl -s "$url" | wc -c)
-
-# Check if the body size is not empty
-if [ -n "$body_size" ]; then
-    echo "$body_size"
-else
-    echo "Unable to determine the body size."
-fi
+# Gets the body size of a request
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
