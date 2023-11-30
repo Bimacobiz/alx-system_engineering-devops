ba19f7fd29aab30c2ef7d27f6d9fdad0c804fd11
Bimacobiz <collinsmahigi@gmail.com>
2023-11-30 12:06:48 -0800

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -7,10 +7,10 @@ fi
 
 url=$1
 
-# Use curl to send a request and get the size of the body
-body_size=$(curl -sI "$url" | grep -i "content-length" | awk '{print $2}' | tr -d '\r\n')
+# Use curl to send a request and get the body, then use wc to count the bytes
+body_size=$(curl -s "$url" | wc -c)
 
-# Check if content-length header is present
+# Check if the body size is not empty
 if [ -n "$body_size" ]; then
     echo "$body_size"
 else
